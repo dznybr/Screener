c9e768515847e1ba75cb41f1ea4927818d07fbcc
dznybr <dznybr@gmail.com>
2021-09-02 19:37:23 +0800

    rus companies, params

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request, redirect, flash, jsonify
 from patterns import patterns
 from binance.client import Client
 from binance.enums import *
+from datetime import datetime
 
 from settings import proxies, api_key, secret_key
 
@@ -100,22 +101,51 @@ def search():
     stocks_list = {}
     params = {}
 
-    with open('datasets/companies.csv') as f:
+    with open('datasets/ruspanies.csv') as f:
         for row in csv.reader(f):
             stocks_list[row[0]] = row[1]
 
     if pattern == 'RSI_MACD_LONG' or pattern == 'RSI_MACD_SHORT':
 
-        interval = request.form.get("interval")
-        interval = 6 if interval is None else int(interval)
+        interval = 6
 
-        rsi_length = int(request.form.get("rsi_length", 14))
-        rsi_oversold = int(request.form.get("rsi_oversold", 30))
-        rsi_overbought = int(request.form.get("rsi_overbought", 70))
+        rsi_length = 14
+        rsi_oversold = 30
+        rsi_overbought = 70
 
-        macd_fast = int(request.form.get("macd_fast", 12))
-        macd_slow = int(request.form.get("macd_slow", 26))
-        mac_signal = int(request.form.get("mac_signal", 9))
+        macd_fast = 12
+        macd_slow = 26
+        mac_signal = 9
+
+        EMA_Green_TimePeriod = 13
+        EMA_Red_TimePeriod = 8
+        EMA_Blue_TimePeriod = 5
+
+        EMA_Green_Offset = 8
+        EMA_Red_Offset = 5
+        EMA_Blue_Offset = 3
+
+        if request.method == "POST":
+            interval = int(request.form.get("interval", interval))
+
+            rsi_length = int(request.form.get("rsi_length", rsi_length))
+            rsi_oversold = int(request.form.get("rsi_oversold", rsi_oversold))
+            rsi_overbought = int(request.form.get("rsi_overbought", rsi_overbought))
+
+            macd_fast = int(request.form.get("macd_fast", macd_fast))
+            macd_slow = int(request.form.get("macd_slow", macd_slow))
+            mac_signal = int(request.form.get("mac_signal", mac_signal))
+
+            EMA_Green_TimePeriod = int(request.form.get("EMA_Green_TimePeriod", EMA_Green_TimePeriod))
+            EMA_Red_TimePeriod = int(request.form.get("EMA_Red_TimePeriod", EMA_Red_TimePeriod))
+            EMA_Blue_TimePeriod = int(request.form.get("mac_signal", EMA_Blue_TimePeriod))
+
+            EMA_Green_Offset = int(request.form.get("EMA_Green_Offset", EMA_Green_Offset))
+            EMA_Red_Offset = int(request.form.get("EMA_Red_Offset", EMA_Red_Offset))
+            EMA_Blue_Offset = int(request.form.get("EMA_Blue_Offset", EMA_Blue_Offset))
+
+            print(f'request.form: {request.form}')
+            print('Got parameters from form')
 
         params['interval'] = interval
         params['rsi_length'] = rsi_length
@@ -135,17 +165,27 @@ def search():
         print(f'macd_slow {macd_slow}')
         print(f'mac_signal {mac_signal}')
 
-        datafiles = os.listdir('datasets/daily')
+        datafiles = os.listdir('datasets/daily_ru')
         for filename in datafiles:
 
-            df = pd.read_csv(f'datasets/daily/{filename}')
+            df = pd.read_csv(f'datasets/daily_ru/{filename}')
             if df.empty:
                 continue
 
             symbol = filename.split('.csv')[0]
+            # print(f'Analyzing  {symbol}')
 
             rsi = talib.RSI(df['Close'], timeperiod=rsi_length)
             macd, macdsignal, _ = talib.MACD(df['Close'], fastperiod=macd_fast, slowperiod=macd_slow, signalperiod=mac_signal)
+
+            # Alligator EMA
+            df['EMA_green'] = talib.EMA(df['Close'], timeperiod=EMA_Green_TimePeriod)
+            df['EMA_red'] = talib.EMA(df['Close'], timeperiod=EMA_Red_TimePeriod)
+            df['EMA_blue'] = talib.EMA(df['Close'], timeperiod=EMA_Blue_TimePeriod)
+
+            df['EMA_green'] = pd.to_numeric(df['EMA_green'].shift(periods=EMA_Green_Offset))
+            df['EMA_red'] = pd.to_numeric(df['EMA_red'].shift(periods=EMA_Red_Offset))
+            df['EMA_blue'] = pd.to_numeric(df['EMA_blue'].shift(periods=EMA_Blue_Offset))
 
             rsi_signal = False
             macd_signal = False
@@ -166,12 +206,20 @@ def search():
                 previous_macd = macd[len(macd)-i-1]
                 previous_macdsignal = macdsignal[len(macd)-i-1]
 
-                if pattern == 'RSI_MACD_LONG' and previous_macdsignal >= previous_macd and current_macdsignal <= current_macd or \
-                   pattern == 'RSI_MACD_SHORT' and previous_macdsignal <= previous_macd and current_macdsignal >= current_macd :
+                if pattern == 'RSI_MACD_LONG' and previous_macdsignal > previous_macd and current_macdsignal < current_macd or \
+                   pattern == 'RSI_MACD_SHORT' and previous_macdsignal < previous_macd and current_macdsignal > current_macd :
                     macd_signal = True
                     macd_value = current_macd
                     macdsignal_value = current_macdsignal
                     macd_offset = 1-i
+                    macd_price = df["Close"][len(df)-i]
+
+                if macd_signal and pattern == 'RSI_MACD_LONG' and previous_macdsignal < previous_macd and current_macdsignal > current_macd or \
+                   macd_signal and pattern == 'RSI_MACD_SHORT' and previous_macdsignal > previous_macd and current_macdsignal < current_macd :
+                    macd_signal = False
+                    macd_value = 0
+                    macdsignal_value = 0
+                    macd_offset = 0
 
                 current_rsi = rsi[len(rsi)-i]
                 if pattern == 'RSI_MACD_LONG' and current_rsi < rsi_oversold or \
@@ -179,17 +227,26 @@ def search():
                     rsi_signal = True
                     rsi_value = current_rsi
                     rsi_offset = 1-i
+                    rsi_price = df["Close"][len(df)-i]
+
+                if rsi_signal and pattern == 'RSI_MACD_LONG' and current_rsi > rsi_oversold or \
+                   rsi_signal and pattern == 'RSI_MACD_SHORT' and current_rsi < rsi_overbought:
+                    rsi_signal = False
+                    rsi_value = 0
+                    rsi_offset = 0
 
             if rsi_signal and macd_signal:
                 stocks[symbol] = {'company': stocks_list[symbol],
                                   'macd': macd_value,
                                   'macdsignal': macdsignal_value,
                                   'macd_offset': macd_offset,
+                                  'macd_price': macd_price,
                                   'rsi': rsi_value,
-                                  'rsi_offset': rsi_offset
+                                  'rsi_offset': rsi_offset,
+                                  'rsi_price': rsi_price
                                   }
 
-                print(f'Company {symbol}: rsi{rsi_offset} - {rsi_value}, macd{macd_offset} - {macd_value}, macdsignal{macd_offset} - {macdsignal_value} ')
+                print(f'Company {symbol}: rsi({rsi_offset}){rsi_value}[{rsi_price}], macd({macd_offset}){macd_value}[{macd_price}], macdsignal({macd_offset}){macdsignal_value}[]')
 
 #            except:
 #                pass
@@ -198,13 +255,15 @@ def search():
 
 @app.route('/snapshot')
 def snapshot():
-    with open('datasets/companies.csv') as f:
+    with open('datasets/ruspanies.csv') as f:
         companies = f.read().splitlines()
         for company in companies:
             symbol = company.split(',')[0]
             print(symbol)
-            df = yf.download(symbol, start="2021-01-01", end="2021-09-01", proxy=proxies)
-            filename = 'datasets/daily/{}.csv'.format(symbol)
+            df = yf.download(symbol, start="2021-01-01", end=datetime.strftime(datetime.now().date(),'%Y-%m-%d'), proxy=proxies)
+            if df.empty:
+                df = yf.download(f'{symbol}.ME', start="2021-01-01", end=datetime.strftime(datetime.now().date(),'%Y-%m-%d'), proxy=proxies)
+            filename = 'datasets/daily_ru/{}.csv'.format(symbol)
             df.to_csv(filename)
             print(f'File {filename} saved')
 
